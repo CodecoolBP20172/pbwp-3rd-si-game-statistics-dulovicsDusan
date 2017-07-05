905c6f86e19db4e712e81378658bbe1c6c14511f
Dusan Dulovics <n3@blacklodge.local>
2017-07-05 21:42:45 +0200

    i need help

--- reports.py
+++ reports.py
@@ -1,2 +1,18 @@
 
-# Report functions
+
+
+def import_files(file_name):
+    data = []
+    files = open(file_name)
+    for line in files:
+        data.append(line.split("\t"))
+
+
+def count_games(file_name):
+    data = import_files(file_name)
+    return len(data)
+
+
+def decide(file_name, year)
+    data = import_files(file_name)
+    
